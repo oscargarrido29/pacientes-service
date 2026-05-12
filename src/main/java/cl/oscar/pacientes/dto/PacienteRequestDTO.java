69d5ee87e86c0719885c189a65298cb2e31012cd
unknown <SSDD@PNAWSA05LC54414.duoc.acad>
2026-05-12 15:12:36 -0400

    Se agrega PacienteRequestDTO

---
+++ src/main/java/cl/oscar/pacientes/dto/PacienteRequestDTO.java
@@ -0,0 +1,13 @@
+package cl.oscar.pacientes.dto;
+
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class PacienteRequestDTO {
+
+    private String nombre;
+    private String documento;
+    private LocalDate fechaNacimiento;
+
+}
